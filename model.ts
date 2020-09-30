a5f149b409c92d58a880d2da70994e2973952873
cvijisekaran <69483209+cvijisekaran@users.noreply.github.com>
2020-09-30 15:45:19 -0400

    Create model.ts

---
+++ model.ts
@@ -0,0 +1,19 @@
+export class ItemSearchCriteria{
+
+  public  upc:string;		
+	public  beverageProductCode:string;
+	public  packageCode:string;	
+	public  countryCode:string;
+	public  pageSize:any;
+	public  firstRecordNo:any
+	public  active:string;	
+	public  supplierId:number;	
+	public  beverageCategoryId:number;	
+	public  packageCategoryId:number;
+	public  packageGroupId:string;	
+	public  customerItemNumber:string;	
+	public  bottlerId:number;
+	public  buId:string;
+
+
+}
